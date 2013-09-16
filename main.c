5ddfdbbf9beb03b64ddd4eb36cb7f70763b84730
Mateusz Świetlicki <mateusz@swietlicki.net>
2013-09-16 20:59:00 +0200

    test

--- main.c
+++ main.c
@@ -9,19 +9,26 @@
 
 #include "i2c_slave.h"
 
+#define CHECK_BIT(var,pos) ((var) & (1<<(pos)))
+
 int main(void){
 	I2C_init(0x10); 
 	
 	sei();
 	
-	DDRB |= _BV(DDB0);
+	DDRB |= _BV(DDB0); 				//LED1
+	DDRB &= ~_BV(DDB1);				//Button
 	
 	while(1){
-		if(i2c_buffer[1] == 1)
-			PORTB |= _BV(PB0);
-		else
+		if(CHECK_BIT(PINA,1))
 			PORTB &= ~_BV(PB0);
-		_delay_ms(500);
+		else
+			PORTB |= _BV(PB0);
+		//if(i2c_buffer[1] == 1)
+		//	PORTB |= _BV(PB0);
+		//else
+		//	PORTB &= ~_BV(PB0);
+		//_delay_ms(500);
 	}
 	
 	return 0;

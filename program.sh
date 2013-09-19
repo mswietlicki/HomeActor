ab272da9ad04627003c901e883e09c0bce30628b
Mateusz Świetlicki <mateusz@swietlicki.net>
2013-09-19 18:16:34 +0200

    Set avr-gcc to 8 Mhz

--- program.sh
+++ program.sh
@@ -5,7 +5,7 @@ if [ -z $1 ]; then
 fi
 FILE=$(echo $1 | sed 's/\..*//')
 rm $FILE.o $FILE.elf $FILE.hex
-avr-gcc -Os -DF_CPU=1000000UL -mmcu=atmega8 -c -o $FILE.o $1
+avr-gcc -Os -DF_CPU=8000000UL -mmcu=atmega8 -c -o $FILE.o $1
 avr-gcc -mmcu=atmega8 $FILE.o -o $FILE.elf
 avr-objcopy -O ihex -R .eeprom $FILE.elf $FILE.hex
 sudo avrdude -p m8 -c usbasp -P usb -U flash:w:$FILE.hex -U lfuse:w:0xe4:m -U hfuse:w:0xd9:m

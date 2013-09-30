6432d5fa78201158c9b15ef9fcd5feda526a7b83
Mateusz Świetlicki <mateusz@swietlicki.net>
2013-09-30 17:09:47 +0200

    Major refactoring plus adding eeprom mirror of main register

--- i2c_slave.h
+++ i2c_slave.h
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/eeprom.h>
 
 //For ATTiny85
 #define DDR_USI             DDRB
@@ -70,7 +71,10 @@
 	(0x0 << USICNT0); \
 }
 
-#define MAX_I2C_BUFFER 32
+#define ReadRegister(address) (Register[address])
+
+#define MAX_Register 32
+#define Default_I2C_Adress 0x10
 
 typedef enum
 {
@@ -82,15 +86,46 @@ typedef enum
 	USI_SLAVE_GET_DATA_AND_SEND_ACK        = 0x05
 } overflowState_t;
 
+typedef enum
+{
+	I2C_ADDRESS			= 0x00,
+	R_OUT1				= 0x01,
+	R_OUT2				= 0x02,
+	R_IN1				= 0x03,
+	R_IN2				= 0x04
+} register_t;
+
 static uint8_t slaveAddress;
 static volatile overflowState_t overflowState;
 
-static uint8_t i2c_buffer[MAX_I2C_BUFFER];
-static uint8_t i2c_pointer;
+static uint8_t EEMEM eeprom_buffor[MAX_Register];
+static uint8_t is_first_run EEMEM;
+static uint8_t Register[MAX_Register] = {0};
+static uint8_t Register_pointer;
+
+void LoadBufforFromEEPROM(){
+	if(is_first_run){
+		eeprom_write_block(&Register[0], &eeprom_buffor[0], MAX_Register); //Reset EEPROM
+		eeprom_write_byte(&eeprom_buffor[I2C_ADDRESS], Default_I2C_Adress); //Set default address
+		eeprom_write_byte(&is_first_run, 0); //Zero first run flag
+	}else{
+		eeprom_read_block(&Register[0], &eeprom_buffor[0], MAX_Register);	//Read register from EEPROM
+	}
+}
+
+void WriteRegister(uint8_t pointer, uint8_t value){
+	Register[pointer] = value;
+	eeprom_write_byte(&eeprom_buffor[pointer], value);
+}
+
+void I2C_init(){
+	LoadBufforFromEEPROM();
+	I2C_init(ReadRegister(I2C_ADDRESS));
+}
 
 void I2C_init(uint8_t ownAddress)
 {
-	i2c_pointer = 0xFF;
+	Register_pointer = 0xFF;
 	slaveAddress = ownAddress;
 
 	DDR_USI |= (1 << PORT_USI_SCL) | (1 << PORT_USI_SDA); // Set SCL and SDA as output
@@ -131,7 +166,7 @@ ISR(USI_START_VECTOR)
 			(1 << USICS1) | (0 << USICS0) | (0 << USICLK) | // Shift Register Clock Source = external, positive edge 4-Bit Counter Source = external, both edges
 			(0 << USITC); //no toggle clock-port pin
 	}
-	
+
 	USISR =
 		(1 << USI_START_COND_INT) | (1 << USIOIF) | //clear interrupt flags - resetting the Start Condition Flag will release SCL
 		(1 << USIPF) |(1 << USIDC) |
@@ -143,64 +178,66 @@ ISR(USI_OVERFLOW_VECTOR)
 	uint8_t data = 0;
 	switch (overflowState)
 	{
-		case USI_SLAVE_CHECK_ADDRESS:
-			if ((USIDR == 0) || ((USIDR >> 1) == slaveAddress))
-			{
-				if (USIDR & 0x01)
-					overflowState = USI_SLAVE_SEND_DATA;
-				else
-				{
-					overflowState = USI_SLAVE_REQUEST_DATA;
-					i2c_pointer = 0xFF;
-				}
-				SET_USI_TO_SEND_ACK();
-			}
+	case USI_SLAVE_CHECK_ADDRESS:
+		if ((USIDR == 0) || ((USIDR >> 1) == slaveAddress))
+		{
+			if (USIDR & 0x01)
+				overflowState = USI_SLAVE_SEND_DATA;
 			else
-				SET_USI_TO_TWI_START_CONDITION_MODE();
-			break;
-
-		case USI_SLAVE_CHECK_REPLY_FROM_SEND_DATA:
-			if (USIDR)
 			{
-				SET_USI_TO_TWI_START_CONDITION_MODE();
-				return;
+				overflowState = USI_SLAVE_REQUEST_DATA;
+				Register_pointer = 0xFF;
 			}
-		case USI_SLAVE_SEND_DATA:
-			if (i2c_pointer == 0xFF)		// No buffer position given, set buffer address to 0
-				i2c_pointer = 0;
-			USIDR = i2c_buffer[i2c_pointer++];
-
-			overflowState = USI_SLAVE_REQUEST_REPLY_FROM_SEND_DATA;
-			SET_USI_TO_SEND_DATA();
-			break;
-			
-		case USI_SLAVE_REQUEST_REPLY_FROM_SEND_DATA:
-			overflowState = USI_SLAVE_CHECK_REPLY_FROM_SEND_DATA;
-			SET_USI_TO_READ_ACK();
-			break;
-			
-		case USI_SLAVE_REQUEST_DATA:
-			overflowState = USI_SLAVE_GET_DATA_AND_SEND_ACK;
-			SET_USI_TO_READ_DATA();
-			break;
-			
-		case USI_SLAVE_GET_DATA_AND_SEND_ACK:
-			data = USIDR;
-			if (i2c_pointer == 0xFF)		// First access, read buffer position
-				if (data < MAX_I2C_BUFFER)		// Check if address within buffer size
-					i2c_pointer = data;		// Set position as received
-				else
-					i2c_pointer = 0;			// Set address to 0		
-			else
-				i2c_buffer[i2c_pointer++] = data;				// Write data to bufferaccess
-
-			overflowState = USI_SLAVE_REQUEST_DATA;
 			SET_USI_TO_SEND_ACK();
-			break;
+		}
+		else
+			SET_USI_TO_TWI_START_CONDITION_MODE();
+		break;
+
+	case USI_SLAVE_CHECK_REPLY_FROM_SEND_DATA:
+		if (USIDR)
+		{
+			SET_USI_TO_TWI_START_CONDITION_MODE();
+			return;
+		}
+	case USI_SLAVE_SEND_DATA:
+		if (Register_pointer == 0xFF)		// No buffer position given, set buffer address to 0
+			Register_pointer = 0;
+		USIDR = ReadRegister(Register_pointer++);
+
+		overflowState = USI_SLAVE_REQUEST_REPLY_FROM_SEND_DATA;
+		SET_USI_TO_SEND_DATA();
+		break;
+
+	case USI_SLAVE_REQUEST_REPLY_FROM_SEND_DATA:
+		overflowState = USI_SLAVE_CHECK_REPLY_FROM_SEND_DATA;
+		SET_USI_TO_READ_ACK();
+		break;
+
+	case USI_SLAVE_REQUEST_DATA:
+		overflowState = USI_SLAVE_GET_DATA_AND_SEND_ACK;
+		SET_USI_TO_READ_DATA();
+		break;
+
+	case USI_SLAVE_GET_DATA_AND_SEND_ACK:
+		data = USIDR;
+		if (Register_pointer == 0xFF)		// First access, read buffer position
+			if (data < MAX_Register)		// Check if address within buffer size
+				Register_pointer = data;		// Set position as received
+			else
+				Register_pointer = 0;			// Set address to 0		
+		else
+		{
+			WriteRegister(Register_pointer++, data);
+		}
+
+		overflowState = USI_SLAVE_REQUEST_DATA;
+		SET_USI_TO_SEND_ACK();
+		break;
 	} 
 
-	if(i2c_pointer != 0xFF && i2c_pointer >= MAX_I2C_BUFFER)
-		i2c_pointer = 0;
+	if(Register_pointer != 0xFF && Register_pointer >= MAX_Register)
+		Register_pointer = 0;
 }
 #endif
 
--- main.c
+++ main.c
@@ -6,40 +6,37 @@
 #include <util/delay.h>
 #include <stdlib.h>
 #include <avr/interrupt.h>
-#include <avr/eeprom.h>
 #include "i2c_slave.h"
 
 #define CHECK_BIT(var,pos) ((var) & (1<<(pos)))
 #define In1 (!CHECK_BIT(PINB,PB4))
 
-#define RIn1 (i2c_buffer[8])
-#define ROut1 (i2c_buffer[0])
-
 #define SetBit(var,pos,val) if(val) var &= ~_BV(pos); else var |= _BV(pos)
 
+void InOutLoop(register_t in_register, register_t out_register, uint8_t in_value){
+	if(ReadRegister(in_register) != in_value)		//On button down
+		if(ReadRegister(out_register) > 0)
+			WriteRegister(out_register, 0);
+		else
+			WriteRegister(out_register, 1);
+	WriteRegister(in_register, in_value);	//Move button value register
+}
+
 int main(void){
-
-	
-
 	sei();
-	I2C_init(0x13); 
-	
-	eeprom_write_byte((uint8_t*)1, 0x10);
-	i2c_buffer[1] = eeprom_read_byte((uint8_t*)1);
-	
-	DDRB |= _BV(DDB3); 				//LED1
+	I2C_init(); 
+
+	DDRB |= _BV(DDB3) | _BV(DDB1); 				//OUT1 and OUT2
 	DDRB &= ~_BV(DDB4);				//Button
 	PORTB |= _BV(PB4);				//pull-up Button
 
 	while(1){
-		if(RIn1 != In1)		//On button down
-			if(ROut1 > 0)
-				ROut1 = 0;
-			else
-				ROut1 = 1;
-		RIn1 = In1;					//Move button value register
+		InOutLoop(R_IN1, R_OUT1, In1);
 
-		SetBit(PORTB, PB3, ROut1);	//Set bit PB0 of PORTB to ROut1
+
+
+		SetBit(PORTB, PB3, ReadRegister(R_OUT1));	//Set bit PB0 of PORTB to ROut1
+		SetBit(PORTB, PB1, ReadRegister(R_OUT2));
 
 		_delay_ms(50);
 	}

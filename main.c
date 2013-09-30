9deb970a0936d5e2b63f66e90a03b25c25158c47
Mateusz Świetlicki <mateusz@swietlicki.net>
2013-09-30 17:47:24 +0200

    Disabling RESET and defining In1 In2 Out1 Out2

--- main.c
+++ main.c
@@ -11,8 +11,24 @@
 #define GetBit(var,pos) ((var) & (1<<(pos)))
 #define SetBit(var,pos,val) if(val) var &= ~_BV(pos); else var |= _BV(pos)
 
-#define In1 (!GetBit(PINB,PB4))
+#define SetPinOutput(pin) \
+{ \
+	DDRB |= _BV(pin); \
+}
 
+#define SetPinInput(pin, pullup) \
+{ \
+	DDRB &= ~_BV(pin); \
+	SetBit(PORTB, pin, pullup); \
+}
+
+#define Out1_Bit 4;
+#define Out2_Bit 1;
+#define In1_Bit 3;
+#define In2_Bit 5;
+
+#define In1 (!GetBit(PINB,In1_Bit))
+#define In2 (!GetBit(PINB,PB5))
 
 void InOutLoop(register_t in_register, register_t out_register, uint8_t in_value){
 	if(ReadRegister(in_register) != in_value)		//On button down
@@ -30,16 +46,18 @@ int main(void){
 	sei();
 	I2C_init(); 
 
-	DDRB |= _BV(DDB3) | _BV(DDB1); 				//OUT1 and OUT2
-	DDRB &= ~_BV(DDB4);				//Button
-	PORTB |= _BV(PB4);				//pull-up Button
+	SetPinOutput(Out1_Bit);
+	SetPinOutput(Out2_Bit);
+
+	SetPinInput(In1_Bit, 1);
+	SetPinInput(In2_Bit, 1);
 
 	while(1){
 		InOutLoop(R_IN1, R_OUT1, In1);
+		InOutLoop(R_IN2, R_OUT2, In2);
 
-
-		SetBit(PORTB, PB3, ReadRegister(R_OUT1));	//Set bit PB0 of PORTB to ROut1
-		SetBit(PORTB, PB1, ReadRegister(R_OUT2));
+		SetBit(PORTB, Out1_Bit, ReadRegister(R_OUT1));	//Set bit PB0 of PORTB to ROut1
+		SetBit(PORTB, Out2_Bit, ReadRegister(R_OUT2));
 
 		_delay_ms(50);
 	}

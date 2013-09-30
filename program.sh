6c89013b7a0d2d103cb3ee695eb59aa85b635cf1
Mateusz Świetlicki <mateusz@swietlicki.net>
2013-09-30 21:39:53 +0200

    removing disabling reset

--- program.sh
+++ program.sh
@@ -6,4 +6,4 @@ rm $FILE.o $FILE.elf $FILE.hex
 avr-gcc -Os -DF_CPU=8000000UL -mmcu=attiny85 -c -o $FILE.o $MAIN
 avr-gcc -mmcu=attiny85 $FILE.o -o $FILE.elf
 avr-objcopy -O ihex -R .eeprom $FILE.elf $FILE.hex
-sudo avrdude -p t85 -c usbasp -P usb -U flash:w:$FILE.hex -U lfuse:w:0xe2:m -U hfuse:w:0x5f:m -U efuse:w:0xff:m
+sudo avrdude -p t85 -c usbasp -P usb -U flash:w:$FILE.hex -U lfuse:w:0xe2:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m

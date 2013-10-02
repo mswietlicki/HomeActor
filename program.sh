3266a3c5bd2a8ed65bc629250071b347275b6165
Mateusz Świetlicki <mateusz@swietlicki.net>
2013-10-02 18:47:45 +0200

    Modify program.sh to compile main.c and usiTwiSlave.c

--- program.sh
+++ program.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-MAIN='main.c'
-FILE=$(echo $MAIN | sed 's/\..*//')
-rm $FILE.o $FILE.elf $FILE.hex
+rm program.o program.elf program.hex
 
-avr-gcc -Os -DF_CPU=8000000UL -mmcu=attiny85 -c -o $FILE.o $MAIN
-avr-gcc -mmcu=attiny85 $FILE.o -o $FILE.elf
-avr-objcopy -O ihex -R .eeprom $FILE.elf $FILE.hex
+avr-gcc -Os -DF_CPU=8000000UL -mmcu=attiny85 -c -o program.o main.c usiTwiSlave.c
+avr-gcc -mmcu=attiny85 program.o -o program.elf
+avr-objcopy -O ihex -R .eeprom program.elf program.hex
 sudo avrdude -p t85 -c usbasp -P usb -U flash:w:$FILE.hex -U lfuse:w:0xe2:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m

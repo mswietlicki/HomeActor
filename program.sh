60a98b44f993f10930ba60afd39e87438c63d7e5
Mateusz Świetlicki <mateusz@swietlicki.net>
2013-10-02 18:55:23 +0200

    Fix avr-gcc multiple files compilation

--- program.sh
+++ program.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-rm program.o program.elf program.hex
+rm main.o main.elf main.hex
 
-avr-gcc -Os -DF_CPU=8000000UL -mmcu=attiny85 -c -o program.o main.c usiTwiSlave.c
-avr-gcc -mmcu=attiny85 program.o -o program.elf
-avr-objcopy -O ihex -R .eeprom program.elf program.hex
-sudo avrdude -p t85 -c usbasp -P usb -U flash:w:$FILE.hex -U lfuse:w:0xe2:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m
+avr-gcc -Os -DF_CPU=8000000UL -mmcu=attiny85 -c main.c usiTwiSlave.c
+avr-gcc -mmcu=attiny85 -o main.elf main.o usiTwiSlave.o
+avr-objcopy -O ihex -R .eeprom main.elf main.hex
+sudo avrdude -p t85 -c usbasp -P usb -U flash:w:main.hex -U lfuse:w:0xe2:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m
